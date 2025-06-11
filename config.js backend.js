20b68a136784e12ab533b069589164af088c34e5
mohan5902 <mohanselvarajh@gmail.com>
2025-06-11 21:44:58 +0530

    Add files via upload

---
+++ config.js backend.js
@@ -0,0 +1,6 @@
+
+module.exports = {
+  mongoURI: 'mongodb+srv://<username>:<password>@cluster0.mongodb.net/<dbname>?retryWrites=true&w=majority',
+  emailUser: 'youremail@gmail.com',
+  emailPass: 'your_app_password'
+};
